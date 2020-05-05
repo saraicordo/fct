66ba7ed9279c67fa5752ae43dfca023fc2279e94
martuuu96 <marta_mm_96@hotmail.com>
2020-05-05 13:16:59 +0200

    Rutas autentificación añadidas

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,8 @@ Route::get('/', function () {
 });
 
 //Ruta autentificación
-Route::get('/login', 'ConnectController@getLogin');
+Route::get('/login', 'ConnectController@getLogin')->name('login');
+Route::post('/login', 'ConnectController@postLogin')->name('login');
+Route::get('/register', 'ConnectController@getRegister')->name('register');
+Route::post('/register', 'ConnectController@postRegister')->name('register');
+Route::get('/logout', 'ConnectController@getLogout')->name('logout');
