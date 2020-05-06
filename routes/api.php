3fa38e8b85edd4bff279738394a76b80aa2b37e9
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2020-05-06 15:43:44 +0200

    Crear Seeds, Resources, Controllers y API

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,34 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+// List of users
+Route::get('users', 'UserController@index');
+
+//List single user
+Route::get('user/{id}', 'UserController@show');
+
+//Create new user
+Route::post('user', 'UserController@store');
+
+//Update user
+Route::put('user', 'UserController@store');
+
+//Delete user
+Route::delete('user/{id}', 'UserController@destroy');
+
+//--------------
+
+//List Categories
+Route::get('categories', 'CategoryController@index');
+
+//List single Category
+Route::get('category/{id}', 'CategoryController@show');
+
+//Create new category
+Route::post('category', 'CategoryController@store');
+
+//Update category
+Route::put('category', 'CategoryController@store');
+
+//Delete Category
+Route::delete('category/{id}', 'CategoryController@destroy');
