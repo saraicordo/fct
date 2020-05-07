7dcd4d5256952493dee34ed1b17faabc0ad6d343
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2020-05-07 12:06:30 +0200

    Añadir API para productos

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,7 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//User Routes
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -31,7 +32,7 @@ Route::put('user', 'UserController@store');
 //Delete user
 Route::delete('user/{id}', 'UserController@destroy');
 
-//--------------
+//Category Routes
 
 //List Categories
 Route::get('categories', 'CategoryController@index');
@@ -47,3 +48,20 @@ Route::put('category', 'CategoryController@store');
 
 //Delete Category
 Route::delete('category/{id}', 'CategoryController@destroy');
+
+
+//Product Routes
+//List all products
+Route::get('products', 'ProductController@index');
+
+//List single product
+Route::get('product/{id}', 'ProductController@show');
+
+//Create new product
+Route::post('product', 'ProductController@store');
+
+//Update product
+Route::put('product', 'ProductController@store');
+
+//Delete product
+Route::delete('product/{id}', 'ProductController@destroy');
