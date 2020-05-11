4ab78f97225018d2b6de75af6831d69fcea90f71
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2020-05-11 14:24:43 +0200

    Añadir opacidad fondo navbar, cambiar icono search, centrar buscador

--- resources/views/web/master.blade.php
+++ resources/views/web/master.blade.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="{{ url('static/css/web.css?v='.time()) }}">
 
     <link rel="stylesheet" href="{{ url('static/css/fonts.css?v='.time()) }}">
+    {{-- Added Icon Link --}}
+    <script src="https://kit.fontawesome.com/9fee74b96c.js" crossorigin="anonymous"></script>
 
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
