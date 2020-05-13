1a31012ff699ff838770463a43041768cbf41bc3
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2020-05-13 12:55:55 +0200

    Modify fields in categories

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -12,24 +12,9 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        Category::create(['name' => 'Mesas']);
-        Category::create(['name' => 'Sillas']);
-        Category::create(['name' => 'Abanicos']);
-        Category::create(['name' => 'Jabones']);
-        Category::create(['name' => 'Cestas']);
-        Category::create(['name' => 'Brazaletes']);
-        Category::create(['name' => 'Collares']);
-        Category::create(['name' => 'Muñecos']);
-        Category::create(['name' => 'Bolsas']);
-        Category::create(['name' => 'Cerámicas']);
-        Category::create(['name' => 'Cervezas']);
-        Category::create(['name' => 'Cinturones']);
-        Category::create(['name' => 'Carpetas']);
-        Category::create(['name' => 'Zapatos']);
-        Category::create(['name' => 'Carteras']);
-        Category::create(['name' => 'Sombreros']);
-        Category::create(['name' => 'Herramientas']);
-        Category::create(['name' => 'Maletines']);
-        Category::create(['name' => 'Maletas']);
+        Category::create(['name' => 'Furniture']);
+        Category::create(['name' => 'Textile']);
+        Category::create(['name' => 'Ceramics']);
+        Category::create(['name' => 'Jewelry']);
     }
 }
