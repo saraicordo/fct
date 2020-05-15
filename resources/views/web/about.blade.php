85be8b00bc0e7f4262edda9b1a7463a4b12daf88
saraicordo <saraicordo@gmail.com>
2020-05-15 20:16:32 +0200

    cambios de about

--- resources/views/web/about.blade.php
+++ resources/views/web/about.blade.php
@@ -6,12 +6,22 @@
 
     <h2>About us</h2>
     <hr class="my-4">
-    <p class="prueba">It uses utility classes for typography and spacing to space content out within the larger container. 
-    It uses utility classes for typography and spacing to space content out within the larger container. 
-    It uses utility classes for typography and spacing to space content out within the larger container. 
-    It uses utility classes for typography and spacing to space content out within the larger container. 
-    It uses utility classes for typography and spacing to space content out within the larger container. 
-    It uses utility classes for typography and spacing to space content out within the larger container. 
-    It uses utility classes for typography and spacing to space content out within the larger container.</p>
-
+    <div class="about">
+        <p>
+             <img src="static/images/about1.jpg"  width="150" height="100" align="left">
+                 In this web project, we offer the artisan a commercial space, as well as mediation between them and the buyers. 
+                 It seems to us an exciting and prosperous work, where we highlight the ethics of fair trade, providing a free trade space for these 
+                small self-employed workers with the freedom that large wholesale stores do not allow.</p>
+    <break>
+         <p>
+    <img src="static/images/about2.jpg"  width="125" height="90" align="right" HSPACE=10 >
+    Our objective is to create a platform so that they can expose their products and that users can acquire them through secure online 
+    payment with the virtual bank POS.</p>
+    <break>
+    <p>
+    <img src="static/images/about3.jpg"  width="175" height="150" align="left" HSPACE=10 >
+        The aim is the possibility of offering a unique product and at the same time bringing small entrepreneurs closer to an online market, 
+        offering them more visibility and at the same time they can capture more benefits in exchange for the unique works increasingly demanded 
+        by tired buyers of repetitive products. from the big markets.</p>
+</div>
 @stop
