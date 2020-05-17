a9aab9ff3291554b4f8d267502c9b08f451b6ad3
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2020-05-17 23:26:51 +0200

    Remove extra $

--- resources/views/web/products/product.blade.php
+++ resources/views/web/products/product.blade.php
@@ -15,7 +15,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Description</h5>
                     <p class="card-text">{{$product->description}}</p>
-                    <span class="price-tag">${{ number_format($product->price / 100, 2) }} €</span>
+                    <span class="price-tag">{{ number_format($product->price / 100, 2) }} €</span>
                     <small class="text-muted float-right">Add to cart</small>
                 </div>
             </div>
