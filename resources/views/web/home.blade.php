89f60191456ea0553d590dc164a2e324c13da29a
Robert Muñoz Marzo <robertmunozmarzo@Roberts-MacBook-Air.local>
2020-05-17 14:19:22 +0200

    Add css changes and fix links for product images

--- resources/views/web/home.blade.php
+++ resources/views/web/home.blade.php
@@ -1,9 +1,7 @@
 @extends ('web.master')
 
-@section('title', 'NombreCategoría')
-
 @section('content')
-    <h1 class="main-title">Welcome! </h1>
+    <h1 class="main-title">Welcome!</h1>
     <hr class="my-4">
     <h4 class="d-block d-md-none">Check out our products</h4>
     <h5 class="d-none d-md-block">Check out some of our latest products from your local artisans.</h5>
@@ -14,7 +12,7 @@
                 <div class="carousel-item {{$i === 0 ? 'active' : ''}}">
                     <a href={{ URL::action('ViewController@product', $product->id) }}>
                         <img
-                            class="d-block img-fluid carousel-image"
+                            class="d-block img-fluid carousel-image hover-shadow"
                             src="{{ asset('static/images/products/' . $product->id . '.png') }}"
                             alt="{{$product->name}}"
                         >
@@ -25,7 +23,7 @@
                 </div>
             @endforeach
         </div>
-        <a class="carousel-control-prev primary-color" href="#productsCarousel" role="button" data-slide="prev">
+        <a class="carousel-control-prev" href="#productsCarousel" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
